e7c9dc96c3b22dae06d7f37abbe6886a1f2e83a7
akechany0122-lang <akechany0122@gmail.com>
2026-08-27 00:02:59 +0900

    修正

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'ultimate-daihugou-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // キャッシュがあればそれを返し、なければネットワークから取得
+        return response || fetch(event.request);
+      })
+  );
+});
